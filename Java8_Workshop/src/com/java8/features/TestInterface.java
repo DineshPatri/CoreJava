c585a7ef6c99bdf1d174da5c8788627b45d4a753
Patri <DP052402@cerner.net>
2020-02-07 10:37:19 +0530

    Java8 - Streams initial checkin

---
+++ Java8_Workshop/src/com/java8/features/TestInterface.java
@@ -0,0 +1,18 @@
+package com.java8.features;
+
+@FunctionalInterface
+public interface TestInterface {
+	public void add();	
+
+	default String sayHi() {
+		return "Hi";
+	}
+	
+	default String sayHello() {
+		return "Hello";
+	}
+	
+	static boolean isTrue() {
+		return true;
+	}
+}
